b0c3e07746ab659c7a2387bdf994d03303ae57bc
jinwood <jinw@protonmail.com>
2023-08-15 20:44:01 +0100

    Implement insert

--- binary-search-tree/index.test.js
+++ binary-search-tree/index.test.js
@@ -6,4 +6,18 @@ describe("binary-search-tree", () => {
 
     expect(bst).toEqual({ root: null });
   });
+
+  it("should insert a left node when the value is less", () => {
+    const bst = new BinarySearchTree();
+
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(15);
+
+    expect(bst.root).toEqual({
+      value: 10,
+      left: { value: 5, left: null, right: null },
+      right: { value: 15, left: null, right: null },
+    });
+  });
 });
